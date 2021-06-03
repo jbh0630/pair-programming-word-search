62918ac312f45e2de000a6864ca4b98213973b13
jbh0630 <jayjo0630@gmail.com>
2021-06-03 04:12:11 +0000

    Pair Programming - Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
+
+const transpose = function(matrix) {
+    let output = [];
+    for (let i = 0; i < matrix.length; i++){
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (output[j] === undefined) output[j] = []; 
+        output[j][i] = matrix[i][j];
+      }
+    }
+    return output;
+  };
+
 const wordSearch = (letters, word) => { 
+    let result = false;
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        }
     }
-}
+    result = false;
+
+    const flippedArray = transpose(letters);
+
+
+    const verticalJoin = flippedArray.map(ls => ls.join(''))
+    for (v of verticalJoin) {
+        if (v.includes(word)) {
+            return true;
+        }
+    }
+    result = false;
+    
+    return result;
+
+};
+
 
 module.exports = wordSearch
